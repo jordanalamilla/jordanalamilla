05bd7e0667350a50fc23c1849cad2d3ebb099763
Jordan Alamilla <jordan.alamilla@gmail.com>
2019-05-19 23:25:44 -0400

    home - added wordpress functionality for latest web projects

--- functions.php
+++ functions.php
@@ -7,3 +7,8 @@ function my_function_admin_bar() {
     return true;
 }
 add_filter('show_admin_bar', 'my_function_admin_bar');
+
+/*
+ * Enable thumbnails
+ */
+add_theme_support( 'post-thumbnails' ); 
--- index.php
+++ index.php
@@ -9,95 +9,60 @@
 
 <div class="container code-projects">
 
-    <div class="row">
-        <div class="col-12 col-md-6 col-lg-4">
-            <div class="light project-code">
+        <!-- Args -->
+        <?php $args = [
+            'post_type'         => 'web_project',
+            'posts_per_page'    => 3,
+        ];
 
-                <!-- IMAGE -->
-                <div class="image" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/projects/art-2018.jpg);"></div>
+        // Query
+        $the_query = new WP_Query( $args );
+            if ( $the_query->have_posts() ) : ?>
 
-                <!-- DETAILS -->
-                <div class="details">
-                    <div class="info">
-                        <h3 class="title">web-project</h3>
-                        <p class="small-text description">Cras mattis consectetur purus sit amet fermentum. Cras mattis consectetur.</p>
-                    </div>
+            <div class="row">
 
-                    <!-- BOTTOM -->
-                    <div class="container bottom-area">
-                        <div class="row">
-                            <div class="col-10 small-text buttons">
-                                <a href="#" class="btn-code-primary">Live Site</a>
-                                <a href="#" class="btn-code-secondary">View Code</a>
-                            </div>
-                            <div class="col-2 language">
-                                <p class="small-text">JS</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <!-- Loop -->
+                <?php while ( $the_query->have_posts() ) : $the_query->the_post();
+                
+                    // Set the theme
+                    $tech   = get_field( 'web_tech' );
+                    $theme  = 'light';
+                    if( $tech == 'PHP' ) { $theme = 'dark'; } ?>
 
-            </div>
-        </div>
-        <div class="col-12 col-md-6 col-lg-4">
-            <div class="dark project-code">
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="<?php echo $theme; ?> project-code">
 
-                <!-- IMAGE -->
-                <div class="image" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/projects/art-2018.jpg);"></div>
+                            <!-- Image -->
+                            <div class="image"
+                                 style="background-image: url('<?php the_post_thumbnail_url(); ?>'"></div>
 
-                <!-- DETAILS -->
-                <div class="details">
-                    <div class="info">
-                        <h3 class="title">web-project</h3>
-                        <p class="small-text description">Cras mattis consectetur purus sit amet fermentum. Cras mattis consectetur.</p>
-                    </div>
+                            <!-- Details -->
+                            <div class="details">
+                                <div class="info">
+                                    <h3 class="title"><?php the_title(); ?></h3>
+                                    <div class="description"><?php the_field( 'web_description' ); ?></div>
+                                </div>
 
-                    <!-- BOTTOM -->
-                    <div class="container bottom-area">
-                        <div class="row">
-                            <div class="col-10 small-text buttons">
-                                <a href="#" class="btn-code-primary">Live Site</a>
-                                <a href="#" class="btn-code-secondary">View Code</a>
-                            </div>
-                            <div class="col-2 language">
-                                <p class="small-text">PHP</p>
+                                <!-- Bottom -->
+                                <div class="container bottom-area">
+                                    <div class="row">
+                                        <div class="col-10 small-text buttons">
+                                            <a href="<?php the_field( 'web_live_link' ); ?>" class="btn-code-primary">Live Site</a>
+                                            <a href="<?php the_field( 'web_repo_link' ); ?>" class="btn-code-secondary">View Code</a>
+                                        </div>
+                                        <div class="col-2 language">
+                                            <p class="small-text"><?php echo $tech; ?></p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-
+                    </div> <!-- Close project -->
+                <?php endwhile; ?> <!-- Close Loop -->
             </div>
-        </div>
-        <div class="col-12 col-md-6 col-lg-4">
-            <div class="light project-code">
 
-                <!-- IMAGE -->
-                <div class="image" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/projects/art-2018.jpg);"></div>
+        <?php wp_reset_postdata(); endif; ?>
 
-                <!-- DETAILS -->
-                <div class="details">
-                    <div class="info">
-                        <h3 class="title">web-project</h3>
-                        <p class="small-text description">Cras mattis consectetur purus sit amet fermentum. Cras mattis consectetur.</p>
-                    </div>
-
-                    <!-- BOTTOM -->
-                    <div class="container bottom-area">
-                        <div class="row">
-                            <div class="col-10 small-text buttons ">
-                                <a href="#" class="btn-code-primary">Live Site</a>
-                                <a href="#" class="btn-code-secondary">View Code</a>
-                            </div>
-                            <div class="col-2 language">
-                                <p class="small-text">JS</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
-</div>
+</div> <!-- Close gallery -->
     
 <?php include( 'footer.php' ); ?>
